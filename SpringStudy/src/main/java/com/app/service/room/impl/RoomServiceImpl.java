bbc0c35ffeaa5fa52a9d71aa4ab1218b2a852089
chobo6 <hongjegue@naver.com>
2026-02-03 13:51:25 +0900

    MVC 구조 Controller, Service, DAO 계층구조 패턴

---
+++ SpringStudy/src/main/java/com/app/service/room/impl/RoomServiceImpl.java
@@ -0,0 +1,42 @@
+package com.app.service.room.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.app.dao.room.RoomDAO;
+import com.app.dto.room.Room;
+import com.app.service.room.RoomService;
+
+@Service
+public class RoomServiceImpl implements RoomService {
+
+	@Autowired //자동 주입
+	RoomDAO roomDAO;
+	
+	//생성자를 통한 의존성 주입
+//	public RoomServiceImpl(RoomDAO roomDAO) {
+//		this.roomDAO = roomDAO;
+//	}
+	
+	//setter 메소드를 통한 의존성 주입
+//	public void setRoomDAO(RoomDAO roomDAO) {
+//		this.roomDAO = roomDAO;
+//	}
+	
+	@Override
+	public List<Room> findRoomList() {
+
+		System.out.println("[Service] 호출 findRoomList");
+		
+		//서비스 로직 
+		// ...
+		
+		// DAO 활용 -> 실제 데이터 조회
+		List<Room> roomList = roomDAO.findRoomList();
+		
+		return roomList;
+	}
+
+}
