e2e0ba1beaa86329fba822b92ec34a1fb63be8ff
chobo6 <hongjegue@naver.com>
2026-02-04 09:40:24 +0900

    사용자(User) 관련 서비스 구조 추가

---
+++ SpringStudy/src/main/java/com/app/dto/user/User.java
@@ -0,0 +1,14 @@
+package com.app.dto.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+	String id;
+	String pw;
+	String name;
+	String userType;
+	// 사용자 계정 구분
+	// Customer	Admin
+	// CUS		ADM
+}
