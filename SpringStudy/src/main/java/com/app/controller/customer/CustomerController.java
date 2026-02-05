19600edc1c47965aa93eb6c5458eeee97765f1b4
chobo6 <hongjegue@naver.com>
2026-02-05 16:06:33 +0900

    세션 로그인 처리 관련 유틸성 클래스 추가

--- SpringStudy/src/main/java/com/app/controller/customer/CustomerController.java
+++ SpringStudy/src/main/java/com/app/controller/customer/CustomerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.app.common.CommonCode;
 import com.app.dto.user.User;
 import com.app.service.user.UserService;
+import com.app.util.LoginManager;
 
 @Controller
 public class CustomerController {
@@ -67,7 +68,8 @@ public class CustomerController {
 			System.out.println(loginUser);
 			
 			//로그인 성공 아이디 세션에 저장
-			session.setAttribute("loginUserId", loginUser.getId());
+//			session.setAttribute("loginUserId", loginUser.getId());
+			LoginManager.setSessionLoginUserId(session, loginUser.getId());
 			
 //			return "redirect:/main";
 			return "redirect:/customer/mypage";
@@ -85,10 +87,12 @@ public class CustomerController {
 		
 		// 로그인을 한 상태면? -> session 에 로그인 사용자 아이디가 존재
 		
-		if(session.getAttribute("loginUserId") != null) {
+//		if(session.getAttribute("loginUserId") != null) {
+		if(LoginManager.isLogin(session)) {
 			
 			//								session 에서 로그인 사용자 ID 확인
-			String loginUserId = session.getAttribute("loginUserId").toString();
+//			String loginUserId = session.getAttribute("loginUserId").toString();
+			String loginUserId = LoginManager.getLoginUserId(session);
 			
 			// DB에서 user 데이터 조회
 			User user = userSerivce.findUserById(loginUserId);
@@ -107,7 +111,8 @@ public class CustomerController {
 	public String signout(HttpSession session) {
 		
 		// 세션 초기화
-		session.invalidate();
+//		session.invalidate();
+		LoginManager.logout(session);
 		
 		return "redirect:/main";
 	}
