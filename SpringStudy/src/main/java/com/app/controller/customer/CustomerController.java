0dc3306b73e3819fd74b26173dfaf2eca814a72b
chobo6 <hongjegue@naver.com>
2026-02-05 15:17:31 +0900

    사용자 마이페이지 추가, 로그아웃 처리

--- SpringStudy/src/main/java/com/app/controller/customer/CustomerController.java
+++ SpringStudy/src/main/java/com/app/controller/customer/CustomerController.java
@@ -80,4 +80,35 @@ public class CustomerController {
 		//로그인 실패 -> 로그인 시도 화면
 	}
 	
+	@GetMapping("/customer/mypage")
+	public String mypage(HttpSession session, Model model) {
+		
+		// 로그인을 한 상태면? -> session 에 로그인 사용자 아이디가 존재
+		
+		if(session.getAttribute("loginUserId") != null) {
+			
+			//								session 에서 로그인 사용자 ID 확인
+			String loginUserId = session.getAttribute("loginUserId").toString();
+			
+			// DB에서 user 데이터 조회
+			User user = userSerivce.findUserById(loginUserId);
+			
+			// view 데이터 전달
+			model.addAttribute("user", user);
+			
+			return "customer/mypage";
+		}
+		
+		// 로그인 안되어있으면? -> 로그인 페이지로 연결
+		return "redirect:/customer/signin";
+	}
+	
+	@GetMapping("/customer/signout")
+	public String signout(HttpSession session) {
+		
+		// 세션 초기화
+		session.invalidate();
+		
+		return "redirect:/main";
+	}
 }
--- SpringStudy/src/main/webapp/WEB-INF/views/main.jsp
+++ SpringStudy/src/main/webapp/WEB-INF/views/main.jsp
@@ -11,9 +11,16 @@
 	<h1>여기는 main 페이지입니다.</h1>
 
 	<div>
-		<button type="button" onClick="location.href='/customer/signup'">회원가입하기</button>
+		<c:if test="${empty loginUserId}">
+			<button type="button" onClick="location.href='/customer/signup'">회원가입하기</button>
+			<button type="button" onClick="location.href='/customer/signin'">로그인</button>
+		</c:if>
 		
-		<button type="button" onClick="location.href='/customer/signin'">로그인</button>
+		<c:if test="${!empty loginUserId}">
+			<p>${loginUserId} 님 환영합니다!</p>
+			<button type="button" onClick="location.href='/customer/signout'">로그아웃</button>
+			<button type="button" onClick="location.href='/customer/mypage'">마이페이지</button>
+		</c:if>
 	</div>
 </body>
 </html>
