0fa279ef310ef8c97feea4b000f601cc78e419b1
chobo6 <hongjegue@naver.com>
2026-02-09 14:19:59 +0900

    관리자 페이지 객실 검색기능 추가 (퀴즈18)

--- SpringStudy/src/main/java/com/app/service/room/RoomService.java
+++ SpringStudy/src/main/java/com/app/service/room/RoomService.java
@@ -3,6 +3,7 @@ package com.app.service.room;
 import java.util.List;
 
 import com.app.dto.room.Room;
+import com.app.dto.room.RoomSearchCondition;
 
 public interface RoomService {
 	
@@ -16,4 +17,5 @@ public interface RoomService {
 	
 	int modifyRoom(Room room);
 
+	List<Room> findRoomListBySearchCondition(RoomSearchCondition roomSearchCondition);
 }
