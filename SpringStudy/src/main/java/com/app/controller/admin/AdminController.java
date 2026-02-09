7df76a2ffda31f47187fbd045c2626f92bf6ea54
chobo6 <hongjegue@naver.com>
2026-02-09 14:03:15 +0900

    관리자 페이지 사용자 상세 페이지, 사용자 정보 수정

--- SpringStudy/src/main/java/com/app/controller/admin/AdminController.java
+++ SpringStudy/src/main/java/com/app/controller/admin/AdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.app.dto.room.Room;
 import com.app.dto.user.User;
+import com.app.dto.user.UserSearchCondition;
 import com.app.service.room.RoomService;
 import com.app.service.user.UserService;
 
@@ -181,10 +182,59 @@ public class AdminController {
 	}
 	
 	@GetMapping("/admin/users")
-	public String users(Model model) {
-		List<User> userList = userService.findUserList();
+	public String users(Model model, UserSearchCondition userSearchCondition) {
+		
+		System.out.println(userSearchCondition);
+		
+		// 검색 조건에 따라서
+		// 검색 조건이 있으면 -> 조건 검색
+		// 검색 조건이 없으면 -> 전체 조회
+		
+//		List<User> userList = userService.findUserList();
+		List<User> userList = userService.findUserListBySearchCondition(userSearchCondition);
+		
 		model.addAttribute("userList", userList);
 		
 		return "admin/users";
+	}
+	
+	// 관리자페이지
+	
+	// 사용자 상세 정보
+	@GetMapping("/admin/user/{id}")
+	public String user(@PathVariable String id, Model model) {
+		
+		User user = userService.findUserById(id);
+		model.addAttribute("user", user);
+		
+		return "admin/user";
+	}
+	
+	// 사용자 정보 변경(수정)
+	@GetMapping("/admin/modifyUser/{id}")
+	public String modifyUser(@PathVariable String id, Model model) {
+		
+		// 수정페이지
+		// 기존에 있는 값 -> view 세팅
+		User user = userService.findUserById(id);
+		model.addAttribute("user", user);
+		
+		return "admin/modifyUser";
+	}
+	
+	@PostMapping("/admin/modifyUser")
+	public String modifyUserAction(User user) {
+		
+		System.out.println("modifyUser 넘어온 값");
+		System.out.println(user);
+		
+		// DB 연동 수정
+		int result = userService.modifyUser(user);
+		
+		if(result > 0) {
+			return "redirect:/admin/user/" + user.getId();
+		} else {
+			return "redirect:/admin/modifyUser/" + user.getId();
+		}
 	}
 }
--- SpringStudy/src/main/webapp/WEB-INF/views/admin/users.jsp
+++ SpringStudy/src/main/webapp/WEB-INF/views/admin/users.jsp
@@ -10,14 +10,35 @@
 <body>
 	<h1>관리자 페이지</h1>
 	<h2>사용자 목록 조회</h2>
+	
+	<div>
+		<form action="" method="get">
+			<p>검색기준</p>
+			<label>아이디 <input type="checkbox" name="id" value="searchId"   ></label>
+			<label>이름 <input type="checkbox" name="name" ></label>
+			
+			<br>
+			<label><input type="radio" name="userType" value="ADM"  >관리자</label>
+			<label><input type="radio" name="userType" value="CUS" >고객(사용자)</label>
+			<br>
+			
+			<label>검색어 : <input type="text" name="searchKeyword" ></label>
+			<button type="submit">검색</button>	
+		</form>
+		
+	</div>
+	
+	<hr>
 
 	<c:forEach var="user" items="${userList}">
 		<p>
-			${user.id} ${user.pw} ${user.name}
-			<c:choose>
-				<c:when test="${user.userType eq 'ADM'}">관리자</c:when>
-				<c:when test="${user.userType eq 'CUS'}">사용자</c:when>
-			</c:choose>
+			<a href="/admin/user/${user.id}">
+				${user.id} ${user.pw} ${user.name}
+				<c:choose>
+					<c:when test="${user.userType eq 'ADM'}">관리자</c:when>
+					<c:when test="${user.userType eq 'CUS'}">사용자</c:when>
+				</c:choose>
+			</a>
 		</p>
 	</c:forEach>
 	
