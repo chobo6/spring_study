325c1cfec0ffe32b594d9a03b11e7499bf316406
chobo6 <hongjegue@naver.com>
2026-03-10 16:43:11 +0900

    [React Spring] JWT 방식 로그인 처리, JWT 라이브러리 의존성추가

--- SpringStudy/src/main/java/com/app/controller/study/rs/ReactSpringAPIController.java
+++ SpringStudy/src/main/java/com/app/controller/study/rs/ReactSpringAPIController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.util.JwtProvider;
 import com.app.util.LoginManager;
 
 @RestController
@@ -123,6 +124,68 @@ public class ReactSpringAPIController {
 			return "is not login";
 		}
 	}
+	
+	@PostMapping("/api/loginJWT")
+	public String loginJWT(@RequestBody Member member, HttpServletRequest request) {
+		
+		System.out.println(member.getId());
+		System.out.println(member.getPw());
+		
+		//JWT   JSON Web Token (인증정보를 담은 문자열)
+		
+		// 입력값 -> DB확인
+		// 정상이다 아니다~
+		
+		
+		//------------로그인 성공했다고 간주
+		
+		//JWT AccessToekn 발행 -> return 전달
+		
+		// pom.xml jwt 관련 라이브러리 의존성 추가
+		
+		//JwtProvider 활용
+		String accessToken = JwtProvider.createAccessToken(member.getId());
+		System.out.println("로그인 아이디: " + member.getId());
+		System.out.println("발급된 JWT AccessToken : " + accessToken);
+		
+		//토큰을 return -> API 호출한 사용자(FE) 전달
+		return accessToken;
+		
+	}
+	
+	@PostMapping("/api/loginCheckJWT")
+	public String loginCheckJWT(HttpServletRequest request) {
+		//token 추출 가져와서 .. -> 유효한 토큰인지 체크 -> 맞으면? id 값이 뭔지 확인 -> 로직
+		//    id기반 -> DB find 조회 -> ....
+		
+		//유효하지 않은 토큰? -> 로그인 ...
+		String accessToken = JwtProvider.extractToken(request);
+		
+		System.out.println("request 에서 추출한 토큰 값 : " + accessToken);
+		
+		if(accessToken == null) {  //토큰이 없다
+			return "no"; // 응답코드... 없을때 어떤 값을 응답  --> FE 처리
+		}
+		
+		
+		//토큰 검증
+		//유효한 토큰인지 확인
+		System.out.println(JwtProvider.isVaildToken(accessToken));
+		
+		//유효한 토큰?
+		if(JwtProvider.isVaildToken(accessToken)) {
+			//id 추출 -> DB조회 -> 현재로그인한 사용자...
+			
+			String userId = JwtProvider.getUserIdFromToken(accessToken);
+			System.out.println("토큰에서 추출한 사용자id 값 : " + userId);
+			
+			return "userId from token : " + userId;
+		}
+		
+		//유효하지 않으면 뭐한다 ~ 로직 ...
+		return "fail";
+	}
+
 }
 
 
